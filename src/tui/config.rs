da5f33938e69860ac4d44319b6aeea5dae35389b
ChirujanoCodding <trdfabris@gmail.com>
2024-03-14 16:47:49 -0300

    style: added more style to the options

--- src/tui/config.rs
+++ src/tui/config.rs
@@ -7,12 +7,16 @@ pub fn generate_config<'config>() -> RenderConfig<'config> {
     let prefix = Styled::new("").with_fg(Color::White);
     let selected = Styled::new("►").with_fg(Color::DarkGrey);
     let skipped = Styled::new("*skipepd*").with_fg(Color::DarkGrey);
+    let option = StyleSheet::empty().with_fg(Color::DarkGrey);
+    let selected_option = StyleSheet::empty().with_fg(Color::Grey);
     let msg = StyleSheet::empty()
         .with_fg(Color::DarkGrey)
         .with_attr(Attributes::BOLD);
     RenderConfig::default_colored()
         .with_prompt_prefix(prefix)
         .with_highlighted_option_prefix(selected)
+        .with_selected_option(Some(selected_option))
+        .with_option(option)
         .with_canceled_prompt_indicator(skipped)
         .with_help_message(msg)
 }
