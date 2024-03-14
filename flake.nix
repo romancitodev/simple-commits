fa2704305ed593e203c64c83f1b17e2aba0a75e6
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-03-13 21:13:45 -0400

    feat: 🔨 add nix flake and rust toolchain file

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Simple tool for conventional commits";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    cranix.url = "github:Lemin-n/cranix/2af6b2e71577bb8836b10e28f3267f2c5342a8fd";
+    crane.url = "github:ipetkov/crane";
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+  # Iterate over Arm, x86 for MacOs 🍎 and Linux 🐧
+    flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems) (
+      system: let
+        simpleCommitsBundle = import ./. {
+          inherit system;
+          pkgs = nixpkgs.legacyPackages.${system};
+          crane = inputs.crane.lib;
+          cranix = inputs.cranix.lib;
+          fenix = inputs.fenix.packages;
+        };
+      in {
+        inherit (simpleCommitsBundle) apps packages devShells;
+      }
+    );
+}
