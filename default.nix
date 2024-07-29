d4b995da248f6664fd22e890696c18cd828fcdd7
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-07-28 20:26:41 -0500

    fix: switch sha256

--- default.nix
+++ default.nix
@@ -26,7 +26,7 @@ let
   # fenix: rustup replacement for reproducible builds
   toolchain = fenix.${system}.fromToolchainFile {
     file = ./rust-toolchain.toml;
-    sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
+    sha256 = "sha256-6eN/GKzjVSjEhGO9FhWObkRFaE1Jf+uqMSdQnb8lcB4=";
   };
   # crane: cargo and artifacts manager
   craneLib = crane.${system}.overrideToolchain toolchain;
